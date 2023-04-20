c151aa5dd325bf222f409cab6ae124403c2cca4f
seojeongseon <slowarirang@gmail.com>
2023-04-20 15:04:38 +0900

    상품 수정 스프링부트 테스트 전환

--- src/test/kotlin/com/example/kotlinproductorderservice/product/ProductServiceTest.kt
+++ src/test/kotlin/com/example/kotlinproductorderservice/product/ProductServiceTest.kt
@@ -3,29 +3,37 @@ package com.example.kotlinproductorderservice.product
 import org.assertj.core.api.AssertionsForClassTypes.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
 
+@SpringBootTest
 class ProductServiceTest {
 
+    @Autowired
     lateinit var productService:ProductService
-
-    lateinit var productPort: ProductPort
 
     @Test
     fun 상품수정(){
+        productService.addProduct(ProductSteps.상품등록요청_생성())
         val productId = 1L
         val request = UpdateProductRequest("상품 수정",2000,DiscountPolicy.NONE)
-        val product = Product("상품명",1000,DiscountPolicy.NONE)
-        productPort = object : ProductPort {
-            override fun save(product: Product){
-            }
-            override fun getProduct(productId:Long):Product{
-                return product
-            }
-        }
-        productService = ProductService(productPort)
+//        val product = Product("상품명",1000,DiscountPolicy.NONE)
+
+//        productPort = object : ProductPort {
+//            override fun save(product: Product){
+//            }
+//            override fun getProduct(productId:Long):Product{
+//                return product
+//            }
+//        }
+//        productService = ProductService(productPort)
+
         productService.updateProduct(productId, request)
-        assertThat(product.name).isEqualTo("상품 수정")
-        assertThat(product.price).isEqualTo(2000)
+
+        val response = productService.getProduct(productId)
+        val productResponse = response.body
+        assertThat(productResponse?.name).isEqualTo("상품 수정")
+        assertThat(productResponse?.price).isEqualTo(2000)
 
     }
 
