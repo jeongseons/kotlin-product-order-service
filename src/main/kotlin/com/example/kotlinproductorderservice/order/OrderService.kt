84d2ede9183168c1768984ccc9454d200160c45b
seojeongseon <slowarirang@gmail.com>
2023-04-20 16:59:28 +0900

    상품 주문 기능 API 테스트 전환

--- src/main/kotlin/com/example/kotlinproductorderservice/order/OrderService.kt
+++ src/main/kotlin/com/example/kotlinproductorderservice/order/OrderService.kt
@@ -1,13 +1,26 @@
 package com.example.kotlinproductorderservice.order
 
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Component
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
-@Component
+@RestController
+@RequestMapping("/orders")
 class OrderService(orderPort: OrderPort) {
     val orderPort = orderPort
-    fun createOrder(request: CreateOrderRequest) {
+
+    @PostMapping
+    fun createOrder(@RequestBody request: CreateOrderRequest):ResponseEntity<Unit> {
         val product = orderPort.getProductById(request.productId)
+
         val order = Order(product, request.quantity)
+
         orderPort.save(order)
+
+        return ResponseEntity.status(HttpStatus.CREATED).build()
     }
 }
